498892b388fa2e95dc13ccf8f446d9011e76037f
Daniel Moran <dmj@tid.es>
2016-04-07 13:05:14 +0200

    ADD Executable for the IoT Agent

--- config.js
+++ config.js
@@ -23,3 +23,32 @@
 
 'use strict';
 
+var config = {};
+
+config.mqtt = {
+    host: 'localhost',
+    port: 1883,
+    defaultKey: '1234'
+};
+
+config.iota = {
+    logLevel: 'DEBUG',
+    contextBroker: {
+        host: 'localhost',
+        port: '1026'
+    },
+    server: {
+        port: 4041
+    },
+    deviceRegistry: {
+        type: 'memory'
+    },
+    types: {},
+    service: 'howtoService',
+    subservice: '/howto',
+    providerUrl: 'http://localhost:4041',
+    deviceRegistrationDuration: 'P1M',
+    defaultType: 'Thing'
+};
+
+module.exports = config;
