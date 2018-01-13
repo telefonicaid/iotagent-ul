21d0d00e0100a30099579496704006f13ea57674
Fernando Mendez Requena <fernando.mendez.external@atos.net>
2018-01-13 14:04:44 +0100

    It supports sending active attributes using ngsiv2

--- test/unit/ngsiv2/httpBindings-test.js
+++ test/unit/ngsiv2/httpBindings-test.js
@@ -489,7 +489,7 @@ iotamMock;
         });
     });
 
-    /*describe('When there is a conflict between configuration and devices', function() {
+    describe('When there is a conflict between configuration and devices', function() {
         var getOptions = {
             url: 'http://localhost:' + config.http.port + '/iot/d',
             method: 'GET',
@@ -547,7 +547,7 @@ iotamMock;
         });
     });
 
-    describe('When a real production request arrives to the IoTA', function() {
+    /*describe('When a real production request arrives to the IoTA', function() {
         var postOptions = {
             url: 'http://localhost:' + config.http.port + '/iot/d',
             method: 'POST',
