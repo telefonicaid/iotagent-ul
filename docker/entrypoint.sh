7568a010d7d07a6c151bdfc5bf4b0a0927bc6081
Jason Fox <jason.fox@fiware.org>
2019-02-20 15:03:10 +0000

    Add header

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+#
+# Copyright 2019 Telefonica Investigación y Desarrollo, S.A.U
+#
+# This file is part of iotagent-ul
+#
+# iotagent-ul is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# iotagent-ul is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with iotagent-ul. If not, see http://www.gnu.org/licenses/.
+#
+# For those usages not covered by the GNU Affero General Public License please contact
+# with iot_support at tid dot es
+#
 
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
